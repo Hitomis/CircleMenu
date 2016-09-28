84679305649c251740275320424721a0064d697b
hitomi <1670957065@qq.com>
2016-09-28 11:39:41 +0800

    Initial commit

---
+++ cmlibrary/src/main/java/com/hitomi/cmlibrary/CircleMenu.java
@@ -0,0 +1,23 @@
+package com.hitomi.cmlibrary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by hitomi on 2016/9/28.
+ */
+public class CircleMenu extends View {
+
+    public CircleMenu(Context context) {
+        this(context, null);
+    }
+
+    public CircleMenu(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CircleMenu(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
