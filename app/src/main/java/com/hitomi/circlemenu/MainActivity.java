483eb57834e33ff8d7a4158c3e92d45584892ad9
hitomi <1670957065@qq.com>
2016-10-08 16:42:13 +0800

    edit demo

--- app/src/main/java/com/hitomi/circlemenu/MainActivity.java
+++ app/src/main/java/com/hitomi/circlemenu/MainActivity.java
@@ -1,13 +1,31 @@
 package com.hitomi.circlemenu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hitomi.cmlibrary.CircleMenu;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CircleMenu circleMenu;
+
+    private int[] iconResArray = new int[5];
+
+    {
+        iconResArray[0] = R.mipmap.icon_home;
+        iconResArray[1] = R.mipmap.icon_search;
+        iconResArray[2] = R.mipmap.icon_notify;
+        iconResArray[3] = R.mipmap.icon_setting;
+        iconResArray[4] = R.mipmap.icon_gps;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        circleMenu = (CircleMenu) findViewById(R.id.circle_menu);
+        circleMenu.setMainIconResource(R.mipmap.icon_menu, R.mipmap.icon_cancel);
+        circleMenu.setSubIconResources(iconResArray);
     }
 }
